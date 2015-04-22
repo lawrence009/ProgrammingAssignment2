1864ca09df23f2f9ee8401cbad7449b086618cea
Lawrence Chen <lawrence.chen@experian.com>
2015-04-22 15:33:58 -0700

    build out more internal functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Prorgramming Assignment 2
 
-## Write a short comment describing this function
+library(MASS)
 
+# creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+    # m is the inverse matrix of x
+    m <- NULL
 
-}
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+    # computes the inverse of the special "matrix" returned by makeCacheMatrix
+    # If the inverse has already been calculated (and the matrix has not changed),
+    # then retrieve the inverse from the cache
+    cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ginv(x)
+    }
+
+
+    set <- function(y) {
+        x <<- y
+        m <<- cacheSolve(y)
+    }
+
+
+    get <- function() {
+        x
+    }
+
+
+    setInverse <- function(matrix) {
+        m <<- matrix
+    }
+
+
+    getInverse <- function() {
+        m
+    }
+
+
+    if(is.matrix(x)) {
+        print('Well done, my young Padawan.')
+        print('I shall create the required matrix.')
+        set(x)
+    } else {
+        stop('Sorry, Neo. I require a matrix.')
+    }
+
+
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
